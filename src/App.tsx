e79708bd2decfe522854d00fec6a7f4ce01463fd
AShuuu-Technoid <maashwin.115@gmail.com>
2025-01-05 01:57:19 +0530

    added underline

--- src/App.tsx
+++ src/App.tsx
@@ -134,6 +134,7 @@ function App() {
         href="https://calendar.app.google/cbS4p9r5QvuCk9dUA" // Replace with actual link to the location or details
         target="_blank"
         rel="noopener noreferrer"
+        className="text-blue-400 hover:underline"
         >
             <p className="text-xs md:text-sm font-bold text-white">Jan 11, Saturday</p>
             </a>
@@ -144,6 +145,7 @@ function App() {
         href="https://calendar.app.google/cbS4p9r5QvuCk9dUA" // Replace with actual link to the location or details
         target="_blank"
         rel="noopener noreferrer"
+        className="text-blue-400 hover:underline"
         >
             <p className="text-xs md:text-sm font-bold text-white">At 6:00 PM</p>
             </a>
@@ -155,6 +157,7 @@ function App() {
         href="https://bit.ly/dhiyash-1st-HBD" // Replace with actual link to the location or details
         target="_blank"
         rel="noopener noreferrer"
+        className="text-blue-400 hover:underline"
       >
               <h2 className="text-xs md:text-sm font-bold text-white">Shivjay Mini Hall</h2>
               </a>
